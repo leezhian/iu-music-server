056c9aeb97db1ccc5bc33d857d106a733abe9daa
kami232 <1347408207@qq.com>
2020-01-09 15:29:16 +0800

    feat: 新增专辑列表接口

--- app/api/v1/album.js
+++ app/api/v1/album.js
@@ -5,11 +5,45 @@ const Router = require('koa-router');
 const Base64 = require('js-base64').Base64;
 
 const {Lyric} = require('../../modules/lyric');
+const {Album} = require('../../modules/album');
+const {Singer} = require('../../modules/singer');
 const {ParameterException} = require('../../../core/http-exception');
 
 // 设置路由前缀
 const router = new Router({
   prefix: '/api/v1/album'
+});
+
+// 获取专辑列表或歌单列表
+router.get('/getRecordList.do', async (ctx, next) => {
+  const query = ctx.request.query;
+  const page = query.page || 1;
+  const pageSize = query.pageSize || 6;
+  const type = query.type || 1;
+
+  if (!Number(page) || !Number(pageSize) || !Number(type)) {
+    throw new ParameterException('参数类型错误', 10003);
+  }
+
+  let data = {};
+  let result;
+  // 获取专辑列表
+  if (type == 1) {
+    result = await Album.selectAlbum(page, pageSize);
+    await Singer.selectSingers(result);
+  } else if (type == 2) {
+
+  }
+  data.list = result || [];
+  // 判断是否有下一页
+  data.hasNextPage = result.length >= pageSize;
+
+  ctx.body = {
+    code: 200,
+    message: '成功',
+    data
+  }
+
 });
 
 // 获取歌词
--- app/modules/album.js
+++ app/modules/album.js
@@ -2,10 +2,35 @@
  * 专辑表
  */
 const {Sequelize, Model} = require('sequelize');
+const moment = require('moment');
 const {sequelize} = require('../../core/db');
 
 class Album extends Model {
+  static async selectAlbum(page, pageSize) {
+    const data = await Album.findAll({
+      attributes: [
+        'id',
+        ['singer_id', 'singer_ids'],
+        ['album_name', 'recordName'],
+        'cover',
+        ['song_ids', 'songIds'],
+        'description',
+        'price',
+        ['buy_total', 'total'],
+        'createAt',
+        'updateAt'
+      ],
+      where: {
+        'isDel': 0
+      },
+      order: [['createAt', 'DESC']],
+      offset: (page - 1) * pageSize,
+      limit: pageSize,
+      raw: true
+    });
 
+    return data;
+  }
 }
 
 Album.init({
@@ -54,13 +79,19 @@ Album.init({
     type: Sequelize.DATE,
     allowNull: false,
     defaultValue: Sequelize.NOW,
-    comment: '创建时间'
+    comment: '创建时间',
+    get() {
+      return moment(this.getDataValue('createAt')).format('YYYY-MM-DD HH:mm:ss');
+    }
   },
   updateAt: {
     type: Sequelize.DATE,
     allowNull: false,
     defaultValue: Sequelize.NOW,
-    comment: '修改时间'
+    comment: '修改时间',
+    get() {
+      return moment(this.getDataValue('updateAt')).format('YYYY-MM-DD HH:mm:ss');
+    }
   },
   isDel: {
     type: Sequelize.INTEGER(1),
